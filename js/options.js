47d2f02707c4596e89ae7b1764940575db4cdc84
mosshan <mosshan@yahoo.com>
2021-11-13 14:46:08 -0500

    Storing and grabbing URLs from storage

--- js/options.js
+++ js/options.js
@@ -8,14 +8,38 @@
 }
 
   // Create list of items
-  // store in 
+  // store in chrome storage
+
+// by passing an object you can define default values e.g.: []
+chrome.storage.local.get({userKeyIds: []}, function (result) {
+  // the input argument is ALWAYS an object containing the queried keys
+  // so we select the key we need
+  var userKeyIds = result.userKeyIds;
+  userKeyIds.push({keyPairId: keyPairId, HasBeenUploadedYet: false});
+  // set the new array value to the same key
+  chrome.storage.local.set({userKeyIds: userKeyIds}, function () {
+      // you can use strings instead of objects
+      // if you don't  want to define default values
+      chrome.storage.local.get('userKeyIds', function (result) {
+          console.log(result.userKeyIds)
+      });
+  });
+});
+  
 
 */
 
 
 let page = document.getElementById("buttonDiv");
 let selectedClassName = "current";
-const presetButtonColors = ["#3aa757", "#e8453c", "#f9bb2d", "#4688f1"];
+const presetURLs = ["https://www.google.com/", "https://replit.com/@mosshan/testy#main.go"];
+
+
+chrome.storage.sync.set({URLs: presetURLs}, function() {
+  console.log('Value is set to' + presetURLs);
+});
+
+
 
 // Reacts to a button click by marking the selected button and saving
 // the selection
@@ -28,34 +52,31 @@ function handleButtonClick(event) {
     current.classList.remove(selectedClassName);
   }
 
+  /*
   // Mark the button as selected
   let color = event.target.dataset.color;
   event.target.classList.add(selectedClassName);
-  chrome.storage.sync.set({ color });
+  chrome.storage.sync.set({ color });*/
 }
 
 // Add a button to the page for each supplied color
-function constructOptions(buttonColors) {
-  chrome.storage.sync.get("color", (data) => {
-    let currentColor = data.color;
-    // For each color we were provided…
-    for (let buttonColor of buttonColors) {
+function constructOptions() {
+
+  chrome.storage.sync.get(['URLs'], function (result) {
+    console.log("get from local storage " + result.URLs)
+    for (let URL of result.URLs) {
       // …create a button with that color…
       let button = document.createElement("button");
-      button.dataset.color = buttonColor;
-      button.style.backgroundColor = buttonColor;
-
-      // …mark the currently selected color…
-      if (buttonColor === currentColor) {
-        button.classList.add(selectedClassName);
-      }
+      button.innerHTML = URL;     
 
       // …and register a listener for when that button is clicked
       button.addEventListener("click", handleButtonClick);
       page.appendChild(button);
     }
   });
+    
+
 }
 
 // Initialize the page by constructing the color options
-constructOptions(presetButtonColors);
+constructOptions();
