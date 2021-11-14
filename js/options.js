9a30523f0e79064de230fc402593cbe9185b188b
mosshan <mosshan@yahoo.com>
2021-11-13 20:31:38 -0500

    Validates URLs

--- js/options.js
+++ js/options.js
@@ -26,18 +26,38 @@ let addURL = document.getElementById("addURL");
 addURL.addEventListener("click", function(){
   var URLList = [];
   var URLtoAdd = document.getElementById("add").value;
-  chrome.storage.sync.get(['URLs'], function (result) {
-    if(result.URLs != undefined){
-      URLList = result.URLs;
-    }
-    URLList.push(URLtoAdd);
-    chrome.storage.sync.set({URLs: URLList}, function() {
-      console.log('Value is set to' + URLList);
+  if(!is_url(URLtoAdd)){
+    console.log("fuck you");
+    alert("Please enter a valid URL");
+  }
+  else{
+    chrome.storage.sync.get(['URLs'], function (result) {
+      if(result.URLs != undefined){
+        URLList = result.URLs;
+      }
+      URLList.push(URLtoAdd);
+      chrome.storage.sync.set({URLs: URLList}, function() {
+        console.log('Value is set to' + URLList);
+      });
     });
-  });
-  location.reload();
+    location.reload();
+  }
 });
 
+function is_url(str)
+{
+  regexp =  /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
+        if (regexp.test(str))
+        {
+          console.log("matches")
+          return true;
+        }
+        else
+        {
+          console.log("matches do not")
+          return false;
+        }
+}
 
 // Create list of URLs which have been added to the blacklist
 function constructOptions() {
